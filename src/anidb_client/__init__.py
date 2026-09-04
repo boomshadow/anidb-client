fbda30b78da69375bf284f42ac5fc20e12f356be
Jacob Tirey <jacob@tirey.io>
2026-09-03 20:08:12 -0400

    chore: release v3.0.0-rc.1

--- src/anidb_client/__init__.py
+++ src/anidb_client/__init__.py
@@ -83,7 +83,7 @@ __all__ = [
 # the wheel filename. See ADR-002 and SPEC-009.
 #
 # Bumped as part of cutting a release, never by hand in the course of other work.
-__version__ = "2.2.0-rc.1"
+__version__ = "3.0.0-rc.1"
 
 # Identity sent to AniDB in the AUTH command. The (name, version) pair must be
 # registered with AniDB before it will authenticate, and `anidb_client_version` is
